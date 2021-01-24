bfaeaa35b5cbed2c76bf7def964a27841b47b0e2
Himesh Wijesooriya <himeshw@salpo.com>
2021-01-24 19:21:22 +0530

    minor style update

--- hello/src/components/SignIn.js
+++ hello/src/components/SignIn.js
@@ -121,7 +121,7 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 100,
-    height: 40,
+    height: 45,
     padding: 10,
     alignItems: 'center',
     justifyContent: 'center',
